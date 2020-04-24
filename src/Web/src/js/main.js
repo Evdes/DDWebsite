b29dc3d44b948790a2a78312fc26c6d11546d553
evdes <Evert.deSchryver@realdolmen.com>
2020-04-24 18:13:08 +0200

    Added contact page

--- src/Web/src/js/main.js
+++ src/Web/src/js/main.js
@@ -1,6 +1,14 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 import "bootstrap";
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 import "../css/app.scss";
+
+$(function(){
+    $("#nav-placeholder").load("navbar.html");
+    $("#footer-placeholder").load("footer.html");
+  });
