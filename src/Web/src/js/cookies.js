c3852108682f17e64cdd8874150fa0d8acf23800
Evert De Schryver <evert.de.schryver@gmail.com>
2020-07-11 14:40:09 +0200

    Updated npm

--- src/Web/src/js/cookies.js
+++
@@ -1,8 +0,0 @@
-if (localStorage.getItem("cookie-consent") != "true") {
-    $(".cookie-banner").delay(500).fadeIn();
-  };
-
-  $(".close-banner").click(function() {
-    localStorage.setItem("cookie-consent", "true");
-    $(".cookie-banner").fadeOut();
-  })
